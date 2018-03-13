4ff2f34297767986a21c79be127d2f161761f989
hys <huyansheng@fishtrip.cn>
2018-03-13 23:19:33 +0800

    Updates

--- src/index.js
+++ src/index.js
@@ -1,119 +1,44 @@
 import React, { Component } from 'react'
-import classnames from 'classnames'
-import {
-  isRequired,
-  number,
-  string,
-  array,
-  arrayOf,
-  oneOfType,
-  shape,
-} from 'prop-types'
-import './index.css'
+import { render } from 'react-dom'
+import PhotoGrid from '../../src'
+import cats from './cats.json'
 
-function getRealImgDimension(url) {
-  if (typeof window !== 'undefined') {
-    let image = new Image()
-    image.src = url
-    return new Promise((resolve, reject) => {
-      image.onload = () => {
-        resolve({
-          url,
-          width: image.width,
-          height: image.height,
-        })
-      }
-      image.onerror = () => {
-        reject()
-      }
-    })
-  }
-  return Promise.resolve({
-    url,
-    width: 1,
-    height: 1,
-  })
-}
-
-function isString(str) {
-  return typeof str === 'string'
-}
-
-class PhotoGrid extends Component {
-  static defaultProps = {
-    unit: 200,
-    photos: [],
-  }
-
-  static propTypes = {
-    className: string,
-    unit: number.isRequired,
-    photos: oneOfType([
-      arrayOf(string).isRequired,
-      arrayOf(
-        shape({
-          url: string.isRequired,
-          width: number.isRequired,
-          height: number.isRequired,
-        })
-      ).isRequired,
-    ]),
-  }
-
-  state = { photoInfos: [] }
-
-  renderPhotos(photoInfos) {
-    const { unit, className } = this.props
-    const photoGridCls = classnames('photo-grid', className)
+class Demo1 extends Component {
+  render() {
+    const photos = cats.map(
+      cat => 'https://xieranmaya.github.io/images/cats/' + cat.url
+    )
     return (
-      <div className={photoGridCls}>
-        {photoInfos.map(photoInfo => {
-          const { width, height, url } = photoInfo
-          return (
-            <div
-              className="photo-grid-wrapper"
-              key={url}
-              style={{
-                width: height ? width / height * unit : 0,
-                flexGrow: height ? width / height * unit : 0,
-              }}>
-              <i
-                style={{
-                  paddingBottom: `${width ? height / width * 100 : 0}%`,
-                }}
-              />
-              <img src={url} />
-            </div>
-          )
-        })}
+      <div>
+        <h1>photo-grid Demo with url</h1>
+        <PhotoGrid photos={photos} />
       </div>
     )
   }
+}
 
+class Demo2 extends Component {
   render() {
-    const { photos } = this.props
-    if (isString(photos[0])) {
-      return this.renderPhotos(this.state.photoInfos)
-    }
-    return this.renderPhotos(photos)
-  }
-
-  loadImages() {
-    this.props.photos.forEach(photo =>
-      getRealImgDimension(photo).then(photoInfo => {
-        this.setState(prevState => {
-          return { photoInfos: [...prevState.photoInfos, photoInfo] }
-        })
-      })
+    const photos = cats.map(cat => ({
+      ...cat,
+      url: 'https://xieranmaya.github.io/images/cats/' + cat.url,
+    }))
+    return (
+      <div>
+        <h1>photo-grid Demo with url and size info</h1>
+        <PhotoGrid photos={photos} />
+      </div>
     )
-  }
-
-  componentDidMount() {
-    const { photos } = this.props
-    if (isString(photos[0])) {
-      this.loadImages()
-    }
   }
 }
 
-export default PhotoGrid
+const App = props => {
+  return (
+    <div>
+      <Demo1 />
+      <Demo2 />
+    </div>
+  )
+}
+
+render(<App />, document.querySelector('#demo'))
